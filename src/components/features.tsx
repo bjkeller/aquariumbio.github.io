0feda0b497baaedecaab3638e8d5e0634e43c7f8
Ben Keller <bjkeller@uw.edu>
2021-03-28 04:50:23 +0000

    Add button

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import { Container } from '@material-ui/core'
 
 export interface FeatureProps {
     children: JSX.Element[] | JSX.Element
 }
 
 const Features = ({ children }: FeatureProps) => (
-    <Grid container>
-        {children}
-    </Grid>
+    <Container>
+        <Grid container>
+            {children}
+        </Grid>
+        <Button variant="contained">
+            Get Started
+        </Button>
+    </Container>
 )
 
 export default Features
