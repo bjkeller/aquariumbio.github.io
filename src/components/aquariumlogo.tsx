06449bfa550aebbc4d0df4bdbc894a38c5909988
Ben Keller <bjkeller@uw.edu>
2021-05-04 03:38:38 +0000

    Modify logo

--- src/components/aquariumlogo.tsx
+++ src/components/aquariumlogo.tsx
@@ -1,38 +1,17 @@
-import React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
-import { Grid, makeStyles, Typography } from '@material-ui/core'
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
 
-const useStyles = makeStyles({
-    textStyle: {
-        fontFamily: 'Lato',
-        fontSize: '32px',
-        fontStyle: 'normal',
-        fontWeight: 400,
-        textAlign: 'center',
-    }
-});
-
-
-
-const AquariumLogo = () => {
-    const classes = useStyles();
-
-    return (
-    <Grid container direction='row' alignItems='center' wrap='nowrap' spacing={2}>
-        <Grid item>
-            <StaticImage
-                src="../images/aq-logo.png"
-                alt="Aquarium logo"
-                width={62}
-                height={62}
-            />
-        </Grid>
-        <Grid item>
-            <Typography variant='h2' className={classes.textStyle}>
-                Aquarium
-            </Typography>
-        </Grid>
-    </Grid>
-)}
+const AquariumLogo = () => (
+    <SvgIcon
+        width={62}
+        height={62}
+    >
+        <svg width="223" height="219" viewBox="0 0 223 219" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="223" height="219" fill="#40D3FD" />
+            <path d="M58.0804 145.215H88.8975L94.9401 168.738H118.053L85.272 56.7029H63.2165L30.4355 168.738H51.8867L58.0804 145.215ZM84.2145 127.422H62.7634L65.7846 115.962C68.957 104.201 71.5251 96.209 73.4889 85.654H73.7911C75.906 96.0583 78.323 104.352 81.3443 116.113L84.2145 127.422Z" fill="white" />
+            <path d="M170.406 198.895H192.764V88.3681H170.406V97.4153C165.572 90.9315 159.076 86.7095 149.861 86.7095C134.151 86.7095 123.576 100.13 123.576 128.93C123.576 157.73 134 169.944 149.71 169.944C159.681 169.944 166.176 164.365 170.406 158.786V198.895ZM158.17 152.603C149.71 152.603 145.934 144.762 145.934 128.779C145.934 112.192 149.861 105.407 158.019 105.407C163.004 105.407 166.932 108.423 170.406 112.343V145.516C167.083 148.984 163.155 152.603 158.17 152.603Z" fill="white" />
+        </svg>
+    </SvgIcon>
+)
 
 export default AquariumLogo
