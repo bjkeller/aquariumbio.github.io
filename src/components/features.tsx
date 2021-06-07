8ebd43c47daa4d9792b3db2d361b9443f27fc07d
Ben Keller <bjkeller@uw.edu>
2021-06-07 20:28:17 +0000

    Update imports

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
-import { Container, makeStyles } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
 
 // TODO: format button
 // TODO: bottom two cards have background images
