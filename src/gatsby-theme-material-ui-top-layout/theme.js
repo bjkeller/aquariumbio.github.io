6a3715ffb6f0cec9ef8f8f391b0ff3b1633d1f69
Mariko Anderson <marikoa@uw.edu>
2021-06-28 10:45:19 -0700

    Button styling

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -13,6 +13,27 @@ const theme = createMuiTheme({
             default: '#FFFFFF',
         },
     },
+    overrides: {
+        MuiButton: {
+            root: {
+                fontStyle: 'normal',
+                fontWeight: 'bold',
+                fontSize: '18px',
+                lineHeight: '26px',
+                textAlign: 'center',
+                boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.317116)',
+                borderRadius: '5px',
+            },
+            contained: {
+                backgroundColor: '#40D3FD',
+                color: '#FFFFFF',
+                '&:hover': {
+                    backgroundColor: '#2399CC',
+                }
+            },
+        },
+    }
+
 })
 
 export default theme
