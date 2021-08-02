860395e1f264e3e375f7d869eff464b0e0e6619b
Mariko Anderson <marikoa@uw.edu>
2021-08-02 15:49:10 -0700

    Add interface Add styled download button Add release details

--- src/components/releasedetails.tsx
+++ src/components/releasedetails.tsx
@@ -2,33 +2,62 @@ import React from "react";
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-
-  },
+const useStyles = makeStyles(() => ({
+  btn: {
+    "&.MuiButton-root": {
+      backgroundColor: '#2399CC',
+      '&:hover': {
+        backgroundColor: '#40D3FD',
+      }
+    },
+  }
 }));
 
-export default function RelaseDetails() {
+export interface releaseDetails {
+  id: string,
+  name: string,
+  version: string,
+  description: string,
+  isLatest: boolean,
+  publishedAt: string,
+  author: string,
+  downloadUrl: string
+}
+
+export interface releaseDetailProps {
+  releaseDetails: releaseDetails,
+}
+
+export default function RelaseDetails({ releaseDetails }: releaseDetailProps) {
   const classes = useStyles();
+  const { id, name, version, description, isLatest, publishedAt, author, downloadUrl } = releaseDetails
 
   return (
-    <Grid item zeroMinWidth xs={12}>
-      <Typography variant="body1" paragraph>
-        <b>version number</b>
-      </Typography>
+    <Grid item zeroMinWidth xs={12} id={id}>
+      { isLatest &&
+        <Typography variant="body1" paragraph>
+          <b>{version}</b>: {name}, {publishedAt}
+        </Typography>
+      }
 
       <Typography variant="body1" paragraph>
         This release includes the following changes:
       </Typography>
 
       <Typography variant="body1" paragraph>
-        TODO: Query github API
+        {description}
       </Typography>
 
       <Typography variant="body1" paragraph>
-
+        Contributions by: @{author}
       </Typography>
+
+      <Button variant="contained" color="primary" href={downloadUrl} className={classes.btn}>
+        { isLatest ? 'download latest version' : `download ${version}`}
+      </Button>
+
     </Grid>
   )
 }
