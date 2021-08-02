44f641f541872eee663ab8846b515e61e7bd9197
Mariko Anderson <marikoa@uw.edu>
2021-08-02 14:51:02 -0700

    Add page link and styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,9 @@ import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core'
 import AquariumLogo from './aquariumlogo'
+import Link from '@material-ui/core/Link';
+import { Link as GatsbyLink } from 'gatsby';
+import Divider from '@material-ui/core/Divider'
 
 const useStyles = makeStyles((theme) => ({
     footerCard: {
@@ -20,29 +23,100 @@ const useStyles = makeStyles((theme) => ({
     },
     copyrightSpacer: {
         height: '9px'
+    },
+    bottomSpacer: {
+        height: '91px'
+    },
+    center: {
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    brandmark: {
+        display: 'inline-flex',
+        alignItems: 'center',
+        textDecoration: 'none',
+        justifySelf: 'center'
+    },
+    aquarium: {
+        FontFamily: 'Lato',
+        FontStyle: 'Regular',
+        FontSize: '32px',
+        LineHeight: '66px',
+        Align: 'Center',
+        VerticalAlign: 'Top',
+        color: '#FFF',
+        flexGrow: 1,
+    },
+    root: {
+        fontFamily: 'Lato',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '20px',
+        lineHeight: '22px',
+        "&.MuiTypography-colorPrimary": {
+        color: '#FFF',
+        },
     }
 }))
-
 
 const Footer = () => {
     const classes = useStyles();
 
     return (
-        <Grid item container  className={classes.footerCard}>
+        <Grid item container justifyContent="center" alignItems="center" className={classes.footerCard}>
             <Grid item xs={12} className={classes.topSpacer} />
-            <Grid item xs={12}>
-                <Typography variant='body2' align='center'>
-                    <AquariumLogo />
-                </Typography>
+
+            <Grid item xs={12} className={classes.center}>
+              <GatsbyLink to='/' className={classes.brandmark}>
+                <AquariumLogo />
+                <Typography variant='h4' classes={{ h4: classes.aquarium}}>Aquarium</Typography>
+              </GatsbyLink>
             </Grid>
+
             <Grid item xs={12}className={classes.midSpacer} />
-            <Grid item xs={12}>Links</Grid>
+
+            <Grid container item xs={12} spacing={1} className={classes.center}>
+                <Grid item>
+                    <Link underline="none" component={GatsbyLink} to="/support" className={classes.root}>
+                        SUPPORT
+                    </Link>
+                </Grid>
+                <Grid item>
+                    <Divider orientation="vertical" />
+                </Grid>
+                <Grid item>
+                    <Link underline="none" component={GatsbyLink} to="/contributors" className={classes.root}>
+                        CONTRIBUTORS
+                    </Link>
+                </Grid>
+                <Grid item>
+                    <Divider orientation="vertical" />
+                </Grid>
+                <Grid item>
+                    <Link underline="none" component={GatsbyLink} to="/license" className={classes.root}>
+                        LICENSE
+                    </Link>
+                </Grid>
+                <Grid item>
+                    <Divider orientation="vertical" />
+                </Grid>
+                <Grid item>
+                    <Link underline="none" component={GatsbyLink} to="/releases" className={classes.root} >
+                        RELEASES
+                    </Link>
+                </Grid>
+            </Grid>
+
             <Grid item xs={12} className={classes.copyrightSpacer} />
+
             <Grid item xs={12}>
                 <Typography variant='body2' className={classes.textStyle} align='center'>
                     {'© 2021 University of Washington  |  info@aquarium.bio'}
                 </Typography>
             </Grid>
+
+            <Grid item xs={12} className={classes.bottomSpacer} />
+
         </Grid>
     )
 }
