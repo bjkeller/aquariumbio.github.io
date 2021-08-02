bb58618af59fc8ab6dbca6821d7f9669550bdca1
Mariko Anderson <marikoa@uw.edu>
2021-08-02 14:07:41 -0700

    Add background images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,21 @@ import Features from "../components/features"
 import Content from "../components/opencontent"
 import Support from "../components/support"
 import { StaticImage } from "gatsby-plugin-image"
+import backgroundImg1 from '../images/backgrounds/bubbles-and-sand.svg'
+import backgroundImg2 from '../images/backgrounds/bubbles-w-gardient.svg'
+import backgroundImg3 from '../images/backgrounds/fish.svg'
+import backgroundImg4 from '../images/backgrounds/plant-and-sand.svg'
 
 export default function IndexRoute() {
   return (
     <Layout>
-
       <Splash />
-      <Features>
+      <Features id="features">
         <Feature
           headline="Compose  Experiments With Confidence"
-          text="Build experimental plans from modular protocols that yield precise 
+          text="Build experimental plans from modular protocols that yield precise
           and predictable directions at the bench. Aquarium protocols increase reproducibility, reduce mistakes, and seamlessly track metadata. "
+          background={backgroundImg1}
         >
           <StaticImage
             src="../images/compose-desktop.svg"
@@ -28,6 +32,7 @@ export default function IndexRoute() {
         <Feature
           headline="Manage Your Lab in One Place"
           text="Realize lab efficiencies by managing research scientists’ inbound experiments, organizing jobs around similar protocol operations, and assigning and overseeing bench work."
+          background={backgroundImg2}
         >
           <StaticImage
             src="../images/manage-desktop.svg"
@@ -39,6 +44,7 @@ export default function IndexRoute() {
         <Feature
           headline="Experiment with Detailed Protocols"
           text="Aquarium gives you context-specific, step-by-step instructions in real time.  That means no more looking for samples, making mistakes in calculations, or forgetting steps."
+          background={backgroundImg3}
         >
           <StaticImage
             src="../images/experiment-desktop.svg"
@@ -50,6 +56,7 @@ export default function IndexRoute() {
         <Feature
           headline="Put Your Data Where It Belongs"
           text="Aquarium captures everything—from imported sample history to job tracking to experiment results—as you go. "
+          background={backgroundImg4}
         >
           <StaticImage
             src="../images/data-desktop.svg"
@@ -62,14 +69,13 @@ export default function IndexRoute() {
       <Content
         headline="Open Content Science"
         text="We believe that the future of research will see coupling the creative energy of small labs
-      with the power and efficiency of experimentation at scale. With Aquarium, scientists can use existing protocols, 
+      with the power and efficiency of experimentation at scale. With Aquarium, scientists can use existing protocols,
       create their own new ones, and share them with peers around the world. "
       />
       <Support
         headline="Ready To Dive In?"
         text="We’re excited to walk you through how Aquarium can meet the needs of your lab."
       />
-
     </Layout>
   )
 }
