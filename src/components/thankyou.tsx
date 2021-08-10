330d6ed405fb41cc8747dd7cb8b3f432daf89d50
Mariko Anderson <marikoa@uw.edu>
2021-08-09 22:21:28 -0700

    Thank you styling

--- src/components/thankyou.tsx
+++ src/components/thankyou.tsx
@@ -3,14 +3,25 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    container: {
+      // marginTop: theme.spacing(1),
+      // background: '#40D3FD',
+      color: '#FFF'
+    },
+
+}));
+
 export interface ThankYouProps {
 
 }
 
 const ThankYou: React.SFC<ThankYouProps> = () => {
+  const classes = useStyles();
+
   return (
-    <Container>
-      <Typography variant="h1" paragraph>
+    <Container className={classes.container}>
+      <Typography variant="h4" paragraph>
         Thank you for contacting us
       </Typography>
       <Typography variant="body1" >
